0c93e0a9076bbda3f948c781ffc2821d1da7cde6
EthMil <efmi222@g.uky.edu>
2016-04-07 20:55:44 -0400

    modified slightly

--- assignment_3/naive_bayes.py
+++ assignment_3/naive_bayes.py
@@ -11,21 +11,25 @@ def splitDataset(dataset, splitRatio):
     trainSize = int(len(dataset) * splitRatio)
     trainSet = []
     copy = list(dataset)
-    print(len(copy))
     while len(trainSet) < trainSize:
         index = random.randrange(len(copy))
         trainSet.append(copy.pop(index))
     return [trainSet, copy]
 
-# def separateByClass(dataset):
-#     separated = {}
-#     for i in range(len(dataset)):
-#         vector = dataset[i]
-#         if (vector[0] not in separated):
+def separateByClass(dataset):
+    separated = {()}
+    for i in range(len(dataset)):
+        vector = dataset[i]
+        if (vector[0] not in separated):
+            separated[vector[-1]] = []
+        separated[vector[-1]].append(vector)
+    return separated
 
-filename = 'carRandom1.data'
+filename = 'carRandom.data'
 
 dataset = parse_input(filename)
-splitRatio = 0.67
+separated = separateByClass(dataset)
+print('Separated instances: {0}').format(separated)
+splitRatio = 0.33
 train, test = splitDataset(dataset, splitRatio)
-# print('Split {0} rows into train with \n\n {1} \n\n\n\nand test with {2}').format(len(dataset), train, test)
+print('Split {0} rows into train with \n\n {1} \n\n\n\nand test with {2}').format(len(dataset), train, test)
